d4662fff8e38025c894f2c274a4cf67c9c50939d
Nikhilesh Yadav <nikhileshofficial@gmail.com>
2020-09-07 19:46:27 +0530

    function patterns are now boud to root pattern object

--- pattern-match.js
+++ pattern-match.js
@@ -1,17 +1,18 @@
 const EQ_LEVEL=(a,b)=>a==b
-function primitive(pattern,object){
-    if (typeof pattern=='function')return !!pattern(object)
+function primitive(pattern,object,root){
+    if (typeof pattern=='function')return !!pattern.call(root,object)
     else return EQ_LEVEL(pattern,object)
 }
-function match(pattern,object)
+function match(pattern,object,root=undefined)
 {
+    if(!root)root=pattern
     try{
-        if(typeof pattern!='object')return primitive(pattern,object)
+        if(typeof pattern!='object')return primitive(pattern,object,root)
         else
         {
             for (let each in pattern)
             {
-                if (!match(pattern[each],object[each]))return false
+                if (!match(pattern[each],object[each],root))return false
             }
             return true
         }
